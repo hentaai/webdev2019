7d23a2e11fb1e01d4be0afd9deb0badc99eb24d5
hentaai <emforimpstuff@gmail.com>
2019-04-13 21:58:40 +0600

    week12lab

--- WEEK12/todo-front/src/app/main/main.component.ts
+++ WEEK12/todo-front/src/app/main/main.component.ts
@@ -12,6 +12,7 @@ export class MainComponent implements OnInit {
   public task_lists: ITaskList[] = []
   public tasks: ITask[] = []
   public loading = false
+  public name: any
 
   constructor(private provider: ProviderService) { }
 
@@ -27,8 +28,30 @@ export class MainComponent implements OnInit {
   getTasks(task_list: ITaskList){
   	this.provider.getTasks(task_list).then(res => {
   		this.tasks = res
-      console.log(this.tasks)
   	})
+  }
+
+  updateTaskList(task_list: ITaskList){
+    this.provider.updateTaskList(task_list).then(res =>
+      console.log(task_list.name + 'updated'))
+  }
+
+  deleteTaskList(task_list: ITaskList){
+    this.provider.deleteTaskList(task_list.id).then(res => {
+      console.log(task_list.name + 'deleted')
+      this.provider.getTaskLists().then(res =>{
+        this.task_lists = res
+      })
+    })
+  }
+
+  createTaskList(){
+    if (this.name !==''){
+      this.provider.createTaskList(this.name).then(res => {
+        this.name = ''
+        this.task_lists.push(res)
+      })
+    }
   }
 
 }
