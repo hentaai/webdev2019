8608d0f8629ce4f63136675b38c264cef6fbe4d0
hentaai <emforimpstuff@gmail.com>
2019-04-24 00:01:32 +0600

    WEEK13

---
+++ WEEK13/todo_back/api/views/__init__.py
@@ -0,0 +1,9 @@
+# from .fbv import task_lists, task_lists_detail, tasks
+# from .cbv import TaskLists, TaskListDetail
+from .generic_cbv import TaskLists, TaskListDetail
+from .authentication import UserList, login, logout
+
+
+
+
+
